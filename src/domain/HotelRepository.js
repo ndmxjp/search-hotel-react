c9f2db0c9bdb4bc3004a47d9843b36b4cb255fa9
saito-takumi <zionnzumudaikunn@gmail.com>
2018-01-01 23:59:55 +0900

    ホテル検索機能を追加

---
+++ src/domain/HotelRepository.js
@@ -0,0 +1,33 @@
+import Rakuten from '../lib/Rakuten';
+import geolib from 'geolib';
+
+const RAKUTEN_APP_ID = '1028808782158042158';
+
+// eslint-disable-next-line import/prefer-default-export
+export const searchHotelByLocation = (location) => {
+  const params = {
+    applicationId: RAKUTEN_APP_ID,
+    datumType: 1,
+    latitude: location.lat,
+    longitude: location.lng,
+  };
+  return Rakuten.Travel.simpleHotelSearch(params)
+    .then(result =>
+      result.data.hotels.map((hotel) => {
+        const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
+        return {
+          id: basicInfo.hotelNo,
+          name: basicInfo.hotelName,
+          url: basicInfo.hotelInformationUrl,
+          thumbUrl: basicInfo.hotelThumbnailUrl,
+          price: basicInfo.hotelMinCharge,
+          reviewAverage: basicInfo.reviewAverage,
+          reviewCount: basicInfo.reviewCount,
+          distance,
+        };
+      }));
+};
